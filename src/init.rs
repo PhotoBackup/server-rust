b0f504e6895fd2f9fd57060248d24223b4f24356
Stéphane Péchard <stephanepechard@gmail.com>
2016-08-04 01:14:07 +0200

    Trying to make strings work...

--- src/init.rs
+++ src/init.rs
@@ -17,36 +17,64 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+extern crate bcrypt;
+extern crate crypto;
+extern crate ini;
 extern crate rpassword;
 
+use self::bcrypt::{DEFAULT_COST, hash};
+use self::crypto::digest::Digest;
+use self::crypto::sha2::Sha512;
+use self::ini::Ini;
 use self::rpassword::read_password;
 use std::io::{self, Write};
 
 
-pub fn pb_init(username: Vec<String>) {
+pub fn pb_init(username: String, path: String) {
     // mediaroot
     print!("The directory where to put the pictures (should be writable by the server you use): ");
     io::stdout().flush().unwrap();
     let mut mediaroot = String::new();
     io::stdin().read_line(&mut mediaroot)
         .expect("ERROR: Failed to read the line you entered!");
-
-    // owner
-    print!("Owner of the directory [www-data]: ");
-    io::stdout().flush().unwrap();
-    let mut owner = String::new();
-    io::stdin().read_line(&mut owner)
-        .expect("ERROR: Failed to read the line you entered!");
     // trim() because we get the carriage return in the string...
-    owner = owner.trim().to_string();
-    if owner.len() == 0 {
-        owner = "www-data".to_string(); // default
-    }
+    mediaroot = mediaroot.trim().to_string();
 
     // password
     print!("The server password: ");
     io::stdout().flush().unwrap();
     let password = read_password().unwrap();
-    println!("The password is: '{}'", password);
 
+    // create sha512 hash and Bcrypt crypted hash
+    let mut hasher = Sha512::new();
+    hasher.input_str(&*password);
+    let pass_sha = hasher.result_str();
+    // limit pass sha length to 72 characters (bcrypt limitation)
+    let limited_pass_sha = &pass_sha[0..71];
+
+    let passhash = match hash(&*limited_pass_sha, DEFAULT_COST) {
+        Ok(h) => h,
+        Err(_) => panic!()
+    };
+
+    // create ini file
+    let section = "photobackup".to_string() + &get_suffix(&username);
+
+    let mut conf = Ini::new();
+    conf.with_section(Some(section.to_owned()))
+        .set("BindAddress", "127.0.0.1")
+        .set("MediaRoot", mediaroot)
+        .set("Password", pass_sha.to_owned())
+        .set("PasswordBcrypt", passhash)
+        .set("Port", "8420");
+    conf.write_to_file(&path).unwrap();
+}
+
+
+pub fn get_suffix(username: &String) -> String {
+    let mut suffix = "".to_string(); // default
+    if !username.is_empty() {
+        suffix = "-".to_string() + username;
+    }
+    return suffix;
 }
--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,27 @@
  */
 
 extern crate docopt;
-//extern crate iron;
+extern crate ini;
+extern crate iron;
+//extern crate params;
 extern crate router;
 extern crate rustc_serialize;
 
 use docopt::Docopt;
-//use iron::prelude::*;
-//use iron::status;
-//use router::Router;
+use iron::modifiers::Redirect;
+use ini::Ini;
+use iron::prelude::*;
+use iron::{status, Url};
+//use params::{Params, Value};
+use router::Router;
+//use std::collections::HashMap;
+use std::env;
+use std::path::PathBuf;
 
 // local
 mod init;
-use init::pb_init;
+use init::{get_suffix, pb_init};
+
 
 // command line documentation
 const USAGE: &'static str = "
@@ -47,12 +56,13 @@ Options:
   --version     Show version.
 ";
 
+
 #[derive(Debug, RustcDecodable)]
 struct Args {
     cmd_init: bool,
     cmd_run: bool,
     cmd_list: bool,
-    arg_username: Vec<String>,
+    arg_username: String,
 }
 
 
@@ -62,26 +72,46 @@ fn main() {
                             .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
 
+    let pb_path = get_path();
+    read_conf(&args.arg_username, &pb_path);
+
     if args.cmd_init {
-        pb_init(args.arg_username);
+        pb_init(args.arg_username, pb_path);
     } else if args.cmd_run {
-        unimplemented!();
+        launch_server(args.arg_username, pb_path);
     } else if args.cmd_list {
         unimplemented!();
     }
 
-    //println!("PhotoBackup client listening on http://");
-    //launch_server();
 }
-/*
-fn launch_server() {
+
+
+fn get_path() -> String {
+    let home_dir = env::home_dir().unwrap();
+    let mut path = PathBuf::from(home_dir);
+    path.push(".photobackup");
+    return path.to_string_lossy().into_owned();
+}
+
+
+fn read_conf(username: &String, path: &String) { //-> HashMap<String, String> {
+    let conf = Ini::load_from_file(path).unwrap();
+    let section_title = "photobackup".to_string() + &get_suffix(&username);
+    let section = conf.section(Some(section_title)).unwrap();
+    //return section;
+}
+
+
+fn launch_server(username: String, path: String) {
 
     // route handlers
-    fn index_handler(req: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "redirect to http://PhotoBackup.github.io")))
+    fn index_handler(_req: &mut Request) -> IronResult<Response> {
+        let url = Url::parse("http://PhotoBackup.github.io").unwrap();
+        Ok(Response::with((status::Found, Redirect(url.clone()))))
     }
 
     fn post_handler(req: &mut Request) -> IronResult<Response> {
+        //validate_password(req);
         Ok(Response::with((status::Ok, "post image => save it!")))
     }
 
@@ -96,6 +126,18 @@ fn launch_server() {
     router.post("/test", test_handler);
 
     // finally run it
-    Iron::new(router).http("localhost:3000").unwrap();
+    println!("PhotoBackup client listening on http://");
+    Iron::new(router).http("127.0.0.1:3000").unwrap();
+}
+
+
+/*fn validate_password(req: &mut Request) {
+    let map = req.get_ref::<Params>().unwrap();
+    match map.find(&["password"]) {
+        Some(&Value::String(ref password)) if password == "Marie" => {
+            Ok(Response::with((iron::status::Ok, "Welcome back, Marie!")))
+        },
+        _ => Ok(Response::with(iron::status::NotFound)),
+    }
 }
 */
